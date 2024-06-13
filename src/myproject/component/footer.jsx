84ec0357018b4fc9e1fa176018888d64efd55b31
darshakpatel7 <152473666+darshakpatel7@users.noreply.github.com>
2024-06-13 14:03:09 +0530

    Add files via upload

---
+++ src/myproject/component/footer.jsx
@@ -0,0 +1,16 @@
+
+
+
+import './Style.css'
+
+export default function Footer() {
+    return (
+        <>
+            <div className="footer">
+                <p>Copyright © 2024 SP Services Limited</p>
+            </div>
+
+
+        </>
+    )
+}
---
+++ src/myproject/component/header.jsx
@@ -0,0 +1,29 @@
+
+
+import './Style.css'
+
+
+
+export default function Header() {
+
+    return (
+        <>
+            <nav>
+                <div className="header">
+                    <div className="logo">
+
+                        <h1>JW Solutions</h1>
+
+                        
+
+
+
+
+
+
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
